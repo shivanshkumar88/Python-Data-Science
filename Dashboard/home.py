d66f2d48682e3dffa100f58863bbc2e017da9397
shivanshkumar88 <kshivansh8823@gmail.com>
2025-01-29 18:40:00 +0530

    yeti

--- Dashboard/home.py
+++ Dashboard/home.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("Welcome to my Dashboard")
+st.markdown( '<h1 style="color:DodgerBlue; text-align:center; border: 4px solid black; background-color:green;font-family:sans serif;margin-bottom :20px"> Titanic Dashboard </h1> ', unsafe_allow_html = True)
 
 st.image("Assets\img.jpg")
